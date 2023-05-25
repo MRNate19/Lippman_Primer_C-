db951724103e928aeb2fda4c2c9aec91e1a9bcb4
Nathan Gardner <njgardner90@outlook.com>
2023-05-25 23:07:40 +0100

    Added explanantion

--- Chapter_01/exercise_1.6.cpp
+++ Chapter_01/exercise_1.6.cpp
@@ -2,10 +2,13 @@
 
 int main()
 {
-    /* Illegal: The first line is a complete statement, and thus the second line
-                expects a cout object.
+    /* Illegal: The first line is a complete statement, thus the second line
+                expects a cout object and so does the third line.
     */
     std::cout << "The sum of " << v1;
               << " and " << v2;
+              << " is " << v1 + v2 << std::endl;
+
+    /* Remove the semi colon at the end of the first two lines and this program works fine :) */
 
 }
